1dea6521bbbdf87fc5ca64c517106f548b4ff714
diego-ramos130 <diego.onethreezero@gmail.com>
2018-10-08 14:24:59 -0700

    finished creature class but no tests yet

---
+++ src/models/account.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jsonWebToken = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const HttpError = require('http-errors');
+
+const HASH_ROUNDS = 8;
+const TOKEN_SEED_LENGTH = 128;
+
+
+const accountSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  tokenSeed: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  character: {
+    type: mongoose.Schema.Types.ObjectId,
+    unique: true,
+    ref: 'character',
+  },
+});
+
+module.exports = mongoose.Model('account', accountSchema);
+
+function pCreateToken() {
+  this.tokenSeed = crypto.randomBytes(TOKEN_SEED_LENGTH).toString('hex');
+
+  return this.save()
+    .then((savedAccount) => {
+      return jsonWebToken.sign({
+        tokenSeed: savedAccount.tokenSeed,
+      }, process.env.SECRET);
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
+
+function pVerifyPassword(password) {
+  return bcrypt.compare(password, this.passwordHash)
+    .then((compareResult) => {
+      if (!compareResult) {
+        throw new HttpError(401, 'Unauthorized');
+      }
+      return this;
+    })
+    .catch(console.error);
+}
+
+accountSchema.methods.pCreateToken = pCreateToken;
+accountSchema.methods.pVerifyPassword = pVerifyPassword;
+const Account = module.exports = mongoose.model('account', accountSchema);
+
+
+Account.create = (username, email, password) => {
+  return bcrypt.hash(password, HASH_ROUNDS)
+    .then((passwordHash) => {
+      password = null; // eslint-disable-line
+      const tokenSeed = crypto.randomBytes(TOKEN_SEED_LENGTH).toString('hex');
+      return new Account({
+        username,
+        email,
+        tokenSeed,
+        passwordHash,
+      }).save();
+    });
+};
