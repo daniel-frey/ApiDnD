1733b6439bc55f026527a52ad17aa1610ad73c17
Tom <tnorth93@gmail.com>
2018-10-08 14:08:01 -0700

    added more properties to fighter class object

--- src/class.js
+++ src/class.js
@@ -4,6 +4,8 @@ const charClass = module.exports = {};
 
 charClass.fighter = {
   name: 'fighter',
+  level: 1,
+  hp: 10,
   abs: {
     str: 15,
     int: 12,
@@ -11,6 +13,14 @@ charClass.fighter = {
     con: 14,
     wis: 10,
     cha: 8,
+  },
+  mod: {
+    str: Math.floor((charClass.fighter.abs.str - 10) / 2),
+    int: Math.floor((charClass.fighter.abs.int - 10) / 2),
+    dex: Math.floor((charClass.fighter.abs.dex - 10) / 2),
+    con: Math.floor((charClass.fighter.abs.con - 10) / 2),
+    wis: Math.floor((charClass.fighter.abs.wis - 10) / 2),
+    cha: Math.floor((charClass.fighter.abs.cha - 10) / 2),
   },
   weapon: '2H Sword',
   ac: 13,
