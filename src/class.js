cd692cc242045805140c8374b91005ee13e061d9
Tom <tnorth93@gmail.com>
2018-10-08 13:50:28 -0700

    created class file with fighter class premade

---
+++ src/class.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const charClass = module.exports = {};
+
+charClass.fighter = {
+  name: 'fighter',
+  abs: {
+    str: 15,
+    int: 12,
+    dex: 13,
+    con: 14,
+    wis: 10,
+    cha: 8,
+  },
+  weapon: '2H Sword',
+  ac: 13,
+};
