7a6503bfd4864c1b8d5d76152e8b530deb8d75d6
Tom <tnorth93@gmail.com>
2018-10-08 14:49:14 -0700

    class constructor mods

--- src/class.js
+++ src/class.js
@@ -8,13 +8,13 @@ module.exports = class charClass {
     this.hp = hp;
     this.weapon = weapon;
     this.ac = ac;
-    this.absMod = function () {
-      this.absMod.str = (abilities.str - 10) / 2;
-      this.absMod.int = (abilities.int - 10) / 2;
-      this.absMod.dex = (abilities.dex - 10) / 2;
-      this.absMod.con = (abilities.con - 10) / 2;
-      this.absMod.wiz = (abilities.wiz - 10) / 2;
-      this.absMod.cha = (abilities.cha - 10) / 2;
+    this.absMod = {
+      str: Math.floor((abilities.str - 10) / 2),
+      int: Math.floor((abilities.int - 10) / 2),
+      dex: Math.floor((abilities.dex - 10) / 2),
+      con: Math.floor((abilities.con - 10) / 2),
+      wiz: Math.floor((abilities.wiz - 10) / 2),
+      cha: Math.floor((abilities.cha - 10) / 2),
     };
   }
 };
