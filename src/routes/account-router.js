d6cb4c8150ca95ba48158181d72b7242c0500315
Tom <tnorth93@gmail.com>
2018-10-09 13:53:26 -0700

    completed some of the account router

--- src/routes/account-router.js
+++ src/routes/account-router.js
@@ -1,1 +1,9 @@
+'use strict';
 
+const expres = require('express');
+const bodyParser = require('body-parser');
+const HttpError = require('http-errors');
+
+const Account = require('../models/account');
+const logger = require('../lib/logger');
+const
