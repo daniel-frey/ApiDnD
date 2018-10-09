18a262ede147117f6d32326a0b4193db99b13c7a
Tom <tnorth93@gmail.com>
2018-10-09 11:18:07 -0700

    created account mock

---
+++ src/__test__/lib/account-mock.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const faker = require('faker');
+const Account = require('../../models/account');
+
+const accountMock = module.exports = {};
+
+accountMock.pCreateMock = () => {
+  const mock = {};
+  mock.request = {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    email: faker.internet.email(),
+  };
+
+  return Account.create(mock.request.username, mock.request.password, mock.request.email)
+    .then((createdAccount) => {
+      mock.account = createdAccount;
+      return createdAccount.pCreateToken();
+    })
+    .then((token) => {
+      mock.token = token;
+      return mock;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+accountMock.pCleanAccountMocks = () => Account.remove();
