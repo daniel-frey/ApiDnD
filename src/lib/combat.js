fc262d6a1e3a25fd09f13d4f2d4cdc22403e8689
diego-ramos130 <diego.onethreezero@gmail.com>
2018-10-09 11:16:32 -0700

    added more tenets to the combat loop, but still requires mocks and testing for benefit

--- src/lib/combat.js
+++ src/lib/combat.js
@@ -9,7 +9,12 @@ function lootPhase() {
 function playerDeath() {
   logger.log(logger.INFO, 'player loses');
 }
-
+function higherMod(absModObj) {
+  if (absModObj.str <= absModObj.dex) {
+    return absModObj.dex;
+  }
+  return absModObj.str;
+}
 
 function isDead(player, enemy) {
   if (player.class.hp <= 0) {
@@ -19,6 +24,7 @@ function isDead(player, enemy) {
   if (enemy.class.hp <= 0) {
     return lootPhase();
   }
+  return false;
 }
 
 function hitRoll(bonus = 0) {
@@ -26,18 +32,21 @@ function hitRoll(bonus = 0) {
 }
 
 function attack(attacker) {
-  if (hitRoll() > attacker.target.class.ac) {
+  if (hitRoll(higherMod()) > attacker.target.class.ac) {
     const damage = attacker.class.weapon.roll();
-    logger.log(logger.INFO, `player deals ${damage} damage to ${attacker.target.class.name}`);
+    logger.log(logger.INFO, `${attacker.class.name} deals ${damage} damage to ${attacker.target.class.name}`);
     attacker.target.class.hp -= damage;
   }
 }
 
 
 function combat(player, enemy) {
+  let rounds = 1;
   while (!isDead(player, enemy)) {
+    logger.log(logger.INFO, `round ${rounds}`);
     attack(player);
     isDead(player, enemy);
     attack(enemy);
+    rounds += 1;
   }
 }
