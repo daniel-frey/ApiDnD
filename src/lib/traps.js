74ec2a7cd3d98d87942ab2e80921edf69129706c
tganyan <tganyan@gmail.com>
2018-10-10 08:46:36 -0700

    adding updated code for traps.js

--- src/lib/traps.js
+++ src/lib/traps.js
@@ -4,27 +4,27 @@ const dice = require('./diceset');
 
 const traps = module.exports = {};
 
-traps.checkRoom = (charObj) => {
+traps.checkRoom = (charObj, stat) => {
+  const rolled = dice.d20.roll();
+  const charMod = charObj.class.absMod[stat] + rolled;
+  const currentTrapDC = charObj.currentRoom.traps.saveDC;
   if (charObj.currentRoom.traps === {}) {
     return 'The room is safe';
   }
-  traps.disableTrap(charObj);
-  // traps.checkForPassages(charObj);
-  return undefined;
+  if (charMod < currentTrapDC) {
+    return 'You are unable to locate any traps, but you feel you should still proceed with caution.';
+  }
+  return traps.disableTrap(charObj);
 };
 
 traps.disableTrap = (charObj, stat) => {
-  const rolled = dice.d20.roll;
+  const rolled = dice.d20.roll();
   const charMod = charObj.class.absMod[stat] + rolled;
-  const currentTrap = charObj.currentRoom.traps.saveDC;
-  if (charMod < currentTrap) {
-    charObj.hp -= 2;
+  const currentTrap = charObj.currentRoom.traps;
+  const currentTrapDC = currentTrap.saveDC;
+  if (charMod < currentTrapDC) {
+    charObj.class.hp -= 2;
+    return `You fail to make a saving throw on disarming the ${currentTrap.description} trap. You are hit for 2 damage. Your health is currently ${charObj.class.hp}`;
   }
+  return `You successfully disarm the ${currentTrap.description} trap. Whew, that was a close one.`;
 };
-
-// traps.checkForPassages = (charObj) => {
-//   if (charObj.currentRoom.secretPassages === {}) {
-//     return 'There are no secret passages';
-//   }
-//
-// };
